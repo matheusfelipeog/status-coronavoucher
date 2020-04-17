49d868f6c0f37e478c2f69847342361d2b041d99
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-04-16 22:03:51 -0300

    Added basic CLI

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 
 # @author: Matheus Felipe <github.com/matheusfelipeog>
 
+import sys
+
+if len(sys.argv) != 3:
+    print('Uso: python main.py cpf sms_token')
+    print('Exp: python main.py 12345678901 445566')
+    sys.exit(0)
+
 if __name__ == '__main__':
     from status_coronavoucher import Coronavoucher
 
-    cpf = 'CPF AQUI'  # Tudo junto, sem espaço
-    sms_token = 'SMS TOKEN AQUI'  # Tudo junto, sem espaço
+    cpf = sys.argv[1]  
+    sms_token = sys.argv[2]
 
     with Coronavoucher(cpf, sms_token) as Cv:
         print(Cv.show_status())
