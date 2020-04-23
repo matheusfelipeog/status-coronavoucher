340559fa5d4b32ab2eeeead3cafeca695d2e9a4c
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-04-23 19:59:03 -0300

    Update cli

--- cli.py
+++ cli.py
@@ -2,18 +2,63 @@
 
 # @author: Matheus Felipe <github.com/matheusfelipeog>
 
-import sys
-
-if len(sys.argv) != 3:
-    print('Uso: python main.py cpf sms_token')
-    print('Exp: python main.py 12345678901 445566')
-    sys.exit(0)
-
 if __name__ == '__main__':
+
+    # Builtins
+    import argparse
+
+    # My Module
     from status_coronavoucher import Coronavoucher
 
-    cpf = sys.argv[1]  
-    sms_token = sys.argv[2]
+    parser = argparse.ArgumentParser(
+            prog='cli.py',
+            usage='python %(prog)s [-h] cpf token [options]',
+            description='Consulte a situação atual do auxílio emergencial.',
+        )
+
+    # Parâmetro CPF, que corresponde aos digitos do CPF da pessoa que irá consultar o status
+    parser.add_argument(
+        'cpf',
+        action='store',
+        type=int,
+        help='os 11 digitos do CPF, sem espaço ou caracteres especiais.',
+        metavar='CPF',
+    )
+
+    # Parâmetro token, que corresponde aos digitos enviados via SMS para o smartphone.
+    parser.add_argument(
+        'token',
+        action='store',
+        type=int,
+        help='os 6 digitos do token SMS que é enviado.',
+        metavar='TOKEN',
+    )
+
+    # Configuração do parâmetro que determina o formato da saída para visualização dos dados
+    parser.add_argument(
+        '-f', '--format',
+        action='store',
+        type=str,
+        help='formato de retorno -> [simple, complete, raw]',
+        metavar='format',
+        default='simple',
+        choices=['simple', 'complete', 'raw']
+    )
+
+    # Aglomera todos os argumentos passados em um objeto,
+    # assim podendo ser acessados como atributos.
+    args = parser.parse_args()
+
+    cpf = args.cpf 
+    sms_token = args.token
 
     with Coronavoucher(cpf, sms_token) as Cv:
-        print(Cv.show_data())
+
+        if args.format == 'simple':
+            print(Cv.show_data())
+
+        elif args.format == 'complete':
+            print(Cv.show_all_data())
+
+        elif args.format == 'raw':
+            print(Cv.get_data())
