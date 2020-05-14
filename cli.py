1c6e45d4bbf9e4e10cfa2389181acc5014109798
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-05-14 15:49:32 -0300

    Update doc of cli.py file

--- cli.py
+++ cli.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
             prog='cli.py',
-            usage='python %(prog)s [-h] cpf nome nasc mae desc [options]',
+            usage='python %(prog)s [-h] cpf token [options]',
             description='Consulte a situação atual do auxílio emergencial.',
         )
 
